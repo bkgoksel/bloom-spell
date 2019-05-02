db266e60cf9e5fab50198209a9688887f467bdc3
Kerem Goksel <bkgoksel@gmail.com>
2019-05-02 01:09:50 -0700

    Formatting

--- spell.py
+++ spell.py
@@ -5,15 +5,13 @@ from typing import List
 from bloom import BloomFilterSet
 
 
-class SpellChecker():
+class SpellChecker:
     """
     A simple SpellChecker that stores vocabularies of arbitrary size using
     a Bloom filter to catch out-of-dictionary words
     """
-    def __init__(
-            self,
-            vocab_file_path: str,
-            filter_size_bytes: int = None) -> None:
+
+    def __init__(self, vocab_file_path: str, filter_size_bytes: int = None) -> None:
         """
         Creates a new SpellChecker, reading vocabulary from a given file
         For fun let's assume the vocabulary doesn't fit in our memory
@@ -22,15 +20,22 @@ class SpellChecker():
         """
         vocab: BloomFilterSet
 
-        with open(vocab_file_path, 'r') as vocab_file:
+        with open(vocab_file_path, "r") as vocab_file:
             vocab_file.seek(0, 2)
             file_size = vocab_file.tell()
-            approximate_num_words = file_size / 8  # Average English word is 8 characters long
+            approximate_num_words = (
+                file_size / 8
+            )  # Average English word is 8 characters long
             vocab_file.seek(0)
             if filter_size_bytes:
-                self.vocab = BloomFilterSet(expected_number_of_entries=approximate_num_words, byte_size=filter_size_bytes)
+                self.vocab = BloomFilterSet(
+                    expected_number_of_entries=approximate_num_words,
+                    byte_size=filter_size_bytes,
+                )
             else:
-                self.vocab = BloomFilterSet(expected_number_of_entries=approximate_num_words)
+                self.vocab = BloomFilterSet(
+                    expected_number_of_entries=approximate_num_words
+                )
             for word in vocab_file:
                 self.vocab.add(word.strip())
 
--- test.py
+++ test.py
@@ -6,45 +6,45 @@ bloom_set = BloomFilterSet(expected_number_of_entries=2)
 assert len(bloom_set._storage) == BloomFilterSet.INITIAL_SIZE
 assert bloom_set.approximate_size == 0
 
-assert(bloom_set._hash('0') == bloom_set._hash('0'))
+assert bloom_set._hash("0") == bloom_set._hash("0")
 
-bloom_set.add('0')
-assert '0' in bloom_set
+bloom_set.add("0")
+assert "0" in bloom_set
 assert bloom_set.approximate_size == 1
-bloom_set.add('asdf')
-assert 'asdf' in bloom_set
-assert '0' in bloom_set
+bloom_set.add("asdf")
+assert "asdf" in bloom_set
+assert "0" in bloom_set
 assert bloom_set.approximate_size == 2
 print(bloom_set.approximate_false_positive_prob)
 
-spell = SpellChecker('test-vocab.txt')
+spell = SpellChecker("test-vocab.txt")
 
-assert 'hello' in spell
-assert 'world' in spell
+assert "hello" in spell
+assert "world" in spell
 
-result = spell.spell_check('hello world wrold')
+result = spell.spell_check("hello world wrold")
 assert result == [False, False, True], result
 
 print(spell.vocab._num_hashes)
 print(spell.vocab.approximate_false_positive_prob)
 
 
-spell = SpellChecker('all_of_english.txt')
+spell = SpellChecker("all_of_english.txt")
 
-assert 'hello' in spell
-assert 'world' in spell
+assert "hello" in spell
+assert "world" in spell
 
-result = spell.spell_check('hello world wrold')
+result = spell.spell_check("hello world wrold")
 assert result == [False, False, True], result
 print(spell.vocab._num_hashes)
 print(spell.vocab.approximate_false_positive_prob)
 
-spell = SpellChecker('all_of_english.txt', filter_size_bytes=256*1024)
+spell = SpellChecker("all_of_english.txt", filter_size_bytes=256 * 1024)
 
-assert 'hello' in spell
-assert 'world' in spell
+assert "hello" in spell
+assert "world" in spell
 
-result = spell.spell_check('hello world wrold')
+result = spell.spell_check("hello world wrold")
 print(result)
 print(spell.vocab._num_hashes)
 print(spell.vocab.approximate_false_positive_prob)
